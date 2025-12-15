8f761817a75206979f20b3a1539dd5a6da033f48
Michael Cris <rosalinmichaelcris@gmail.com>
2025-12-15 22:03:48 +0800

    changed banner logo and h1 font

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -1,12 +1,16 @@
 export default function Banner() {
   return (
     <div className="banner-div">
-      <h1 className="text-2xl sm:text-5xl playwrite-hu font-bold text-center my-10 bg-green-300 rounded w-9/12 mx-auto py-6">
+      {/* <h1 className="text-2xl sm:text-5xl times-new-roman font-bold text-center my-10 bg-green-300 rounded w-9/12 mx-auto py-6">
         PHARM NCD PATIENT LIST
+      </h1> */}
+      <h1 className="text-2xl sm:text-5xl times-new-roman font-bold text-center my-10 bg-green-300 rounded w-9/12 mx-auto py-6">
+        RHU-MANAOAG PHARMACY
       </h1>
       <img
         // src="/images/pharm-banner.jpg"
-        src="/images/the-best-rhu-friends.jpg"
+        // src="/images/the-best-rhu-friends.jpg"
+         src="/images/the-logo.jpg"
         alt="Pharm Banner"
         className="banner-img mx-auto"
       />
