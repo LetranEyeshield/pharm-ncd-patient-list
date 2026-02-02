7dacf72d8e7921cba15be455d374054c46de5d64
Michael Cris <rosalinmichaelcris@gmail.com>
2026-02-02 08:46:35 +0800

    changed the banner image

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -4,13 +4,13 @@ export default function Banner() {
       {/* <h1 className="text-2xl sm:text-5xl times-new-roman font-bold text-center my-10 bg-green-300 rounded w-9/12 mx-auto py-6">
         PHARM NCD PATIENT LIST
       </h1> */}
-      <h1 className="text-2xl sm:text-5xl times-new-roman font-bold text-center my-10 bg-green-300 rounded w-9/12 mx-auto py-6">
+      <h1 className="text-2xl sm:text-5xl times-new-roman font-bold text-center mt-10 mb-2 bg-green-300 rounded w-9/12 mx-auto py-6">
         RHU-MANAOAG PHARMACY
       </h1>
       <img
         // src="/images/pharm-banner.jpg"
         // src="/images/the-best-rhu-friends.jpg"
-         src="/images/the-logo.jpg"
+         src="/images/logo.jpg"
         alt="Pharm Banner"
         className="banner-img mx-auto"
       />
